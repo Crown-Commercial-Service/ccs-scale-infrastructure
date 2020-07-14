4d2f8929bcd07fe67a657f3f1f491d55fe0b8627
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-07-14 11:15:56 +0100

    SFAT-192 - PPD and PRD environments

---
+++ ccs-scale-infra-shared/terraform/environments/prd/main.tf
@@ -0,0 +1,36 @@
+#########################################################
+# Environment: PRD (Production)
+#
+# Deploy SCALE resources
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-shared-prd"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment = "PRD"
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source                              = "../../modules/configs/deploy-all"
+  aws_account_id                      = data.aws_ssm_parameter.aws_account_id.value
+  environment                         = local.environment
+  cloudtrail_cw_log_retention_in_days = 30
+  cloudtrail_s3_log_retention_in_days = 30
+  cloudwatch_s3_force_destroy         = false
+}
