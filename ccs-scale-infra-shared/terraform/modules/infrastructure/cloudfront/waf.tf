c00c9ef5184f7f13d67801319abc5d2b203f993c
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-08-17 17:10:54 +0100

    SFAT-302 - added Rate Limiting rule to WAF (1000/5min) (#32)

--- ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/waf.tf
+++ ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/waf.tf
@@ -3,11 +3,33 @@
 #
 # Placeholder - rules TBD
 #########################################################
+resource "aws_waf_rate_based_rule" "rate_limiting_rule" {
+  name        = "rateLimitWAFRule"
+  metric_name = "rateLimitWAFRule"
+
+  rate_key   = "IP"
+  rate_limit = 1000
+}
+
 resource "aws_waf_web_acl" "buyer_ui" {
   name        = "SCALE-EU2-${upper(var.environment)}-EXT-FatBuyerUI"
   metric_name = "wafBuyerUi"
 
+  depends_on = [
+    aws_waf_rate_based_rule.rate_limiting_rule,
+  ]
+
   default_action {
     type = "ALLOW"
+  }
+
+  rules {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 1
+    rule_id  = aws_waf_rate_based_rule.rate_limiting_rule.id
+    type     = "RATE_BASED"
   }
 }
