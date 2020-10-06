f66cf0df25253d465a00328de550fd005c025e94
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-10-06 11:23:52 +0100

    Spark and Cognizant CIDR ranges for bastion host external access (#43) (#44)
    * Retrieve and use additional Spark external range SSM for bastion
    
    * Add Cognizant external IPs to bastion host
    
    * Handle null value '-' in CIDR block SSM params
    
    Co-authored-by: Tom Bunting <6031257+tommyb82@users.noreply.github.com>

--- ccs-scale-infra-shared/terraform/modules/configs/deploy-all/main.tf
+++ ccs-scale-infra-shared/terraform/modules/configs/deploy-all/main.tf
@@ -49,8 +49,23 @@ data "aws_ssm_parameter" "bastion_kms_key_id" {
   name = "${lower(var.environment)}-bastion-encryption-key"
 }
 
-data "aws_ssm_parameter" "cidr_blocks_allowed_external" {
-  name = "${lower(var.environment)}-cidr-blocks-external-allowed"
+data "aws_ssm_parameter" "cidr_blocks_allowed_external_ccs" {
+  name = "${lower(var.environment)}-cidr-blocks-allowed-external-ccs"
+}
+
+data "aws_ssm_parameter" "cidr_blocks_allowed_external_spark" {
+  name = "${lower(var.environment)}-cidr-blocks-allowed-external-spark"
+}
+
+data "aws_ssm_parameter" "cidr_blocks_allowed_external_cognizant" {
+  name = "${lower(var.environment)}-cidr-blocks-allowed-external-cognizant"
+}
+
+locals {
+  # Normalised CIDR blocks (accounting for 'none' i.e. "-" as value in SSM parameter)
+  cidr_blocks_allowed_external_ccs       = data.aws_ssm_parameter.cidr_blocks_allowed_external_ccs.value != "-" ? split(",", data.aws_ssm_parameter.cidr_blocks_allowed_external_ccs.value) : []
+  cidr_blocks_allowed_external_spark     = data.aws_ssm_parameter.cidr_blocks_allowed_external_spark.value != "-" ? split(",", data.aws_ssm_parameter.cidr_blocks_allowed_external_spark.value) : []
+  cidr_blocks_allowed_external_cognizant = data.aws_ssm_parameter.cidr_blocks_allowed_external_cognizant.value != "-" ? split(",", data.aws_ssm_parameter.cidr_blocks_allowed_external_cognizant.value) : []
 }
 
 module "infrastructure" {
@@ -83,7 +98,7 @@ module "bastion" {
   subnet_id                    = split(",", data.aws_ssm_parameter.public_web_subnet_ids.value)[0]
   db_cidr_blocks               = split(",", data.aws_ssm_parameter.cidr_blocks_db.value)
   bastion_kms_key_id           = data.aws_ssm_parameter.bastion_kms_key_id.value
-  cidr_blocks_allowed_external = split(",", data.aws_ssm_parameter.cidr_blocks_allowed_external.value)
+  cidr_blocks_allowed_external = concat(local.cidr_blocks_allowed_external_ccs, local.cidr_blocks_allowed_external_spark, local.cidr_blocks_allowed_external_cognizant)
 }
 
 # CloudTrail is not really required in lower enviromnments.
