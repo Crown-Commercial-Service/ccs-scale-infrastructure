58859cd0b4c958aca9f17f9a3614f77e4ac7d156
tommyb82 <thomasbunting@gmail.com>
2020-10-28 14:03:38 +0000

    Remove experimental SG rule - not required

--- ccs-scale-infra-shared/terraform/modules/bastion/main.tf
+++ ccs-scale-infra-shared/terraform/modules/bastion/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "allow_bastion_db_access" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.db_cidr_blocks
   }
 }
 
