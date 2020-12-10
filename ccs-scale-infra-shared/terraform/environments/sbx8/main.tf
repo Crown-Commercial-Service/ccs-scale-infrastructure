094b1891a18cd73502475f4b16e6c63e59658cbf
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-12-10 09:57:01 +0000

    SINF-235 - SBX8 configuration

---
+++ ccs-scale-infra-shared/terraform/environments/sbx8/main.tf
@@ -0,0 +1,33 @@
+#########################################################
+# Environment: SBX8
+#
+# Deploy SCALE resources
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-shared-sbx8"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment = "SBX8"
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source         = "../../modules/configs/deploy-all"
+  aws_account_id = data.aws_ssm_parameter.aws_account_id.value
+  environment    = local.environment
+}
---
+++ ccs-scale-infra-shared/terraform/environments/sbx8/outputs.tf
@@ -0,0 +1,15 @@
+output "lb_private_arn" {
+  value = module.deploy.lb_private_arn
+}
+
+output "lb_public_alb_arn" {
+  value = module.deploy.lb_public_alb_arn
+}
+
+output "vpc_link_id" {
+  value = module.deploy.vpc_link_id
+}
+
+output "lb_private_dns" {
+  value = module.deploy.lb_private_dns
+}
