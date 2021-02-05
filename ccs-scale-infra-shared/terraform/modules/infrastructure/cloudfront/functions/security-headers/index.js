d3f60b45439a9a70de9c63e598e2beeb2988dad8
tommyb82 <thomasbunting@gmail.com>
2021-02-05 10:12:47 +0000

    CSS and S3 domains in content-sec-policy img-src

--- ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/security-headers/index.js
+++ ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/security-headers/index.js
@@ -1,35 +1,48 @@
-'use strict';
+"use strict";
 exports.handler = (event, context, callback) => {
-
   //Get contents of response
   const response = event.Records[0].cf.response;
   const headers = response.headers;
 
   //Set new headers
-  headers['strict-transport-security'] = [{
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubdomains; preload'
-  }];
-  headers['content-security-policy'] = [{
-    key: 'Content-Security-Policy',
-    value: "default-src 'none'; img-src 'self' *.crowncommercial.gov.uk; script-src 'self' 'unsafe-inline'; font-src fonts.gstatic.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; object-src 'none'"
-  }];
-  headers['x-content-type-options'] = [{
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  }];
-  headers['x-frame-options'] = [{
-    key: 'X-Frame-Options',
-    value: 'DENY'
-  }];
-  headers['x-xss-protection'] = [{
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
-  }];
-  headers['referrer-policy'] = [{
-    key: 'Referrer-Policy',
-    value: 'same-origin'
-  }];
+  headers["strict-transport-security"] = [
+    {
+      key: "Strict-Transport-Security",
+      value: "max-age=63072000; includeSubdomains; preload",
+    },
+  ];
+  // Image source requires CCS and S3 domains as BaT product images are loaded via a redirect to an S3 pre-signed URL
+  headers["content-security-policy"] = [
+    {
+      key: "Content-Security-Policy",
+      value:
+        "default-src 'none'; img-src 'self' *.crowncommercial.gov.uk *.s3.eu-west-2.amazonaws.com; script-src 'self' 'unsafe-inline'; font-src fonts.gstatic.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; object-src 'none'",
+    },
+  ];
+  headers["x-content-type-options"] = [
+    {
+      key: "X-Content-Type-Options",
+      value: "nosniff",
+    },
+  ];
+  headers["x-frame-options"] = [
+    {
+      key: "X-Frame-Options",
+      value: "DENY",
+    },
+  ];
+  headers["x-xss-protection"] = [
+    {
+      key: "X-XSS-Protection",
+      value: "1; mode=block",
+    },
+  ];
+  headers["referrer-policy"] = [
+    {
+      key: "Referrer-Policy",
+      value: "same-origin",
+    },
+  ];
 
   //Return modified response
   callback(null, response);
