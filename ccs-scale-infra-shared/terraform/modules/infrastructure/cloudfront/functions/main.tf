8c052b0619901b55172ec4fea614b0f8c972ff77
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-03-10 08:49:05 +0000

    Feature/sinf 369 csp header (#68)
    * SINF-369 - Content Security Policy - working (WIP)
    
    * SINF-369 - dynamic parameter name
    
    * SINF-369 - added 'connect-src' to cover XHR error on content sec policy
    
    * SINF-369 - comment tidy
    
    * SINF-369 - removed test logging

--- ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/main.tf
+++ ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/main.tf
@@ -4,6 +4,9 @@
 # 1) Add Security Headers
 #########################################################
 
+locals {
+  function_name = "scale-${var.resource_label}-${lower(var.environment)}-security-headers"
+}
 # Aliased provider for us-east-1 region for use by specific resources (e.g. ACM certificates)
 provider "aws" {
   alias  = "lambda_edge"
@@ -30,6 +33,15 @@ resource "aws_iam_role" "lambda_edge_exec" {
 EOF
 }
 
+data "aws_iam_policy" "ssm_read_only_access" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_ro_attach" {
+  role       = aws_iam_role.lambda_edge_exec.name
+  policy_arn = data.aws_iam_policy.ssm_read_only_access.arn
+}
+
 data "aws_iam_policy" "lambda_basic_exec" {
   arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
@@ -51,11 +63,19 @@ resource "aws_lambda_function" "security_headers" {
 
   filename         = "${path.module}/.build/security-headers.zip"
   source_code_hash = data.archive_file.lambda_security_headers_zip.output_base64sha256
-  function_name    = "scale-${var.resource_label}-${lower(var.environment)}-security-headers"
+  function_name    = local.function_name
   role             = aws_iam_role.lambda_edge_exec.arn
   description      = "Add HTTP security headers to responses"
   handler          = "index.handler"
   runtime          = "nodejs12.x"
   timeout          = 10
   publish          = true
+}
+
+# Parameter that will be read by security_headers Lambda@edge function
+resource "aws_ssm_parameter" "csp" {
+  name      = "/bat/${local.function_name}-csp"
+  type      = "String"
+  value     = var.content_security_policy
+  overwrite = true
 }
