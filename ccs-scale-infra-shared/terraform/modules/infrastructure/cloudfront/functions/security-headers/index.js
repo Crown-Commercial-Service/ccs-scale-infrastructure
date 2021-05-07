c89327a6c852770eec056d76ea46e0086c958df4
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-05-07 10:34:57 +0100

    Fix  issue with google analytics (#73)
    * Fix  issue with google analytics

--- ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/security-headers/index.js
+++ ccs-scale-infra-shared/terraform/modules/infrastructure/cloudfront/functions/security-headers/index.js
@@ -12,7 +12,8 @@ exports.handler = (event, context, callback) => {
   }];
   headers['content-security-policy'] = [{
     key: 'Content-Security-Policy',
-    value: "default-src 'none'; img-src 'self'; script-src 'self' 'unsafe-inline'; font-src fonts.gstatic.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; object-src 'none'"
+    value: "default-src 'none'; img-src 'self' www.googletagmanager.com; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com; font-src fonts.gstatic.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; object-src 'none'"
+
   }];
   headers['x-content-type-options'] = [{
     key: 'X-Content-Type-Options',
