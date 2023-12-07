eaf27e444f9d50acab06af8a28f62cc059a6b318
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2023-12-07 11:48:08 +0000

    add wildcard to cloudtrail value

--- ccs-scale-infra-shared/terraform/modules/cloudtrail/main.tf
+++ ccs-scale-infra-shared/terraform/modules/cloudtrail/main.tf
@@ -163,7 +163,7 @@ resource "aws_cloudtrail" "scale" {
   s3_bucket_name                = aws_s3_bucket.cloudtrail.id
   include_global_service_events = true
   enable_log_file_validation    = true
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail.arn
 }
 
